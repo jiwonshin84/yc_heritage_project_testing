f536a5dc7df5aa7bc80fffc1755d9e8c769fbf0e
jiwonshin84 <jiwonshin84@gmail.com>
2026-05-07 18:45:26 +0900

    Update 7_🧠_AI_문화재_해설.py

--- pages/7_🧠_AI_문화재_해설.py
+++ pages/7_🧠_AI_문화재_해설.py
@@ -13,7 +13,7 @@ st.set_page_config(
 # =====================================================
 # Gemini API 설정
 # =====================================================
-GEMINI_API_KEY = "여기에_API_KEY_입력"
+GEMINI_API_KEY = "AIzaSyAUJzBLV6EDCWTROatmV9ohhzBWlVANWNw"
 
 genai.configure(
     api_key=GEMINI_API_KEY
