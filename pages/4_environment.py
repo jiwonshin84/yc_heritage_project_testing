32bfcbf0b618a26ff10ae333c7d9df4a7fb3d288
jiwonshin84 <jiwonshin84@gmail.com>
2026-05-07 10:30:58 +0900

    Update 4_environment.py

--- pages/4_environment.py
+++ pages/4_environment.py
@@ -3,31 +3,3 @@ import pandas as pd
 import plotly.express as px
 
 st.title("🌦 환경 데이터 분석")
-
-temp_df = pd.read_csv(
-    "data/environment/온습도데이터.csv"
-)
-
-dust_df = pd.read_csv(
-    "data/environment/미세먼지데이터.csv"
-)
-
-st.subheader("온도 변화")
-
-fig1 = px.line(
-    temp_df,
-    x="date",
-    y="temp"
-)
-
-st.plotly_chart(fig1)
-
-st.subheader("미세먼지 변화")
-
-fig2 = px.line(
-    dust_df,
-    x="date",
-    y="pm10"
-)
-
-st.plotly_chart(fig2)
