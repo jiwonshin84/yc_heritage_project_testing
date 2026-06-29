3dd8e027e308b4696f9a6eb8abf9e69f48e9a8c2
ycshkim <26-3201@sc.gyo6.net>
2026-06-29 15:50:55 +0900

    Update 4_김경은.py

--- pages/4_김경은.py
+++ pages/4_김경은.py
@@ -1,7 +1,7 @@
 import os
+import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import streamlit as st
 
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler
@@ -9,7 +9,13 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-DATA_PATH = os.path.join(BASE_DIR, "data", "yc_heritage_feature.csv")
+
+DATA_PATH = os.path.join(
+    BASE_DIR,
+    "data",
+    "processed",
+    "yc_heritage_feature.csv"
+)
 
 df = pd.read_csv(DATA_PATH, encoding="utf-8-sig")
 
@@ -39,13 +45,15 @@ X_pca = pca.fit_transform(X)
 
 st.title("문화재 군집분석")
 
-st.subheader("군집 결과")
+st.subheader("군집분석 결과")
 
 st.dataframe(
     df[
         [
             "문화재명(국문)",
             "문화재연령",
+            "국가유산종목",
+            "시대그룹",
             "재질",
             "노출형태",
             "Cluster"
@@ -55,21 +63,23 @@ st.dataframe(
 
 st.subheader("군집별 문화재 개수")
 
-st.bar_chart(df["Cluster"].value_counts().sort_index())
+cluster_count = df["Cluster"].value_counts().sort_index()
 
-fig, ax = plt.subplots(figsize=(8,6))
+st.bar_chart(cluster_count)
+
+fig, ax = plt.subplots(figsize=(8, 6))
 
 scatter = ax.scatter(
-    X_pca[:,0],
-    X_pca[:,1],
+    X_pca[:, 0],
+    X_pca[:, 1],
     c=df["Cluster"],
     cmap="viridis",
     s=80
 )
 
+ax.set_title("K-Means Clustering")
 ax.set_xlabel("PCA1")
 ax.set_ylabel("PCA2")
-ax.set_title("K-Means Clustering")
 
 plt.colorbar(scatter)
 
