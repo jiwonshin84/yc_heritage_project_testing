f90c453b2533f18af7d7eb5b46fcb8a8b687a174
ycshkim <26-3201@sc.gyo6.net>
2026-08-15 16:11:21 +0900

    Update ddddd.py

--- pages/ddddd.py
+++ pages/ddddd.py
@@ -15,9 +15,10 @@ import pandas as pd
 # GitHub Secrets에 등록한 API 키 불러오기
 ASOS_SERVICE_KEY = os.getenv("ASOS_SERVICE_KEY")
 
-if not ASOS_SERVICE_KEY:"feb2bfabd299d5d05e89c7aec49ba7e706112603e76549a92e868bd86ec60323"
+if not ASOS_SERVICE_KEY:
+    ASOS_SERVICE_KEY = "feb2bfabd299d5d05e89c7aec49ba7e706112603e76549a92e868bd86ec60323"
 
-    ASOS_URL = (
+ASOS_URL = (
     "http://apis.data.go.kr/"
     "1360000/AsosDalyInfoService/getWthrDataList"
 )
