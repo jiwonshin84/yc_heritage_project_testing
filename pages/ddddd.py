0cca8610a82304007cad6d96cb648ee45f157e4d
ycshkim <26-3201@sc.gyo6.net>
2026-08-27 19:14:14 +0900

    Update ddddd.py

--- pages/ddddd.py
+++ pages/ddddd.py
@@ -90,7 +90,6 @@ try:
     ASOS_SERVICE_KEY = st.secrets["ASOS_SERVICE_KEY"]
 except Exception:
     ASOS_SERVICE_KEY = "feb2bfabd299d5d05e89c7aec49ba7e706112603e76549a92e868bd86ec60323"
-
 ASOS_URL = (
     "http://apis.data.go.kr/1360000/"
     "AsosDalyInfoService/getWthrDataList"
